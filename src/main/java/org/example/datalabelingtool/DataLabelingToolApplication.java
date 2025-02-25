89269da94de0fcb51b9e77db3c5ea01547361978
Andrew Kim <andrew75313@gmail.com>
2025-02-25 14:42:58 +0900

    git init

---
+++ src/main/java/org/example/datalabelingtool/DataLabelingToolApplication.java
@@ -0,0 +1,13 @@
+package org.example.datalabelingtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataLabelingToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataLabelingToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/datalabelingtool/DataLabelingToolApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.datalabelingtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataLabelingToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
