a0ed01deb149bcad8d7d9b5669f05fe2e9efa22f
Andrew Kim <andrew75313@gmail.com>
2025-03-05 15:10:41 +0900

    fix: labels endpoint -labels to labels

--- src/main/java/org/example/datalabelingtool/domain/labels/controller/LabelController.java
+++ src/main/java/org/example/datalabelingtool/domain/labels/controller/LabelController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/lables")
+@RequestMapping("/api/labels")
 public class LabelController {
 
     private final LabelService labelService;
