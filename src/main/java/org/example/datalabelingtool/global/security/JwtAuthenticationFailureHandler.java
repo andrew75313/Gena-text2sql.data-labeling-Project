0aa065540bef46f3f2dbc8dd801682a5f099918d
Andrew Kim <andrew75313@gmail.com>
2025-04-17 18:29:44 +0900

    update: customize error messages for login failures in AuthenticationFailureHandler

--- src/main/java/org/example/datalabelingtool/global/security/JwtAuthenticationFailureHandler.java
+++ src/main/java/org/example/datalabelingtool/global/security/JwtAuthenticationFailureHandler.java
@@ -3,7 +3,9 @@ package org.example.datalabelingtool.global.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
@@ -16,11 +18,21 @@ public class JwtAuthenticationFailureHandler implements AuthenticationFailureHan
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
 
+        String msg;
+
+        if (exception instanceof UsernameNotFoundException) {
+            msg = "Can't find user.";
+        } else if (exception instanceof BadCredentialsException) {
+            msg = "Check your username and password again.";
+        } else {
+            msg = "Fail to Login.";
+        }
+
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json;charset=UTF-8");
 
         String json = new ObjectMapper().writeValueAsString(
-                Map.of("statusCode", 401, "msg", "Fail to Login.")
+                Map.of("statusCode", 401, "msg", msg)
         );
 
         response.getWriter().write(json);
